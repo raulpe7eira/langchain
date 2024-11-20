c0d0770b4b903ad96c94536a9f42c0d9a8628699
Raul Pereira <raul.pereira@gmail.com>
2024-11-20 00:50:20 -0300

    clean import

--- langchain_lcel_join.py
+++ langchain_lcel_join.py
@@ -2,8 +2,6 @@ import os
 
 from dotenv import load_dotenv
 
-from langchain.chains import LLMChain
-from langchain.chains import SimpleSequentialChain
 from langchain_core.output_parsers import JsonOutputParser, StrOutputParser
 from langchain_core.pydantic_v1 import Field, BaseModel
 from langchain.globals import set_debug
